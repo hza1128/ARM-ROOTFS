e9b54a9f0e3d0302270c7af5fe998c54be12e6df
hza1128 <141570215+hza1128@users.noreply.github.com>
2025-05-27 00:11:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git kwrt https://github.com/kiddin9/kwrt-packages' feeds.conf.default
+#sed -i '$a src-git kwrt https://github.com/kiddin9/kwrt-packages' feeds.conf.default
 
